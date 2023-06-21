33b6c6395220bf63e623545bf024c90842021f27
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2023-06-21 16:56:15 +0600

    getter setter in typescript

--- src/access-modifiers.ts
+++ src/access-modifiers.ts
@@ -1,28 +1,29 @@
-class BankAccount {
-  public readonly id: number;
-  public name: string;
-  private _balance: number;
+// class BankAccount {
+//   public readonly id: number;
+//   public name: string;
+//   private _balance: number;
 
-  constructor(id: number, name: string, balance: number) {
-    this.id = id;
-    this.name = name;
-    this._balance = balance;
-  }
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this._balance = balance;
+//   }
 
-  getBalance() {
-    console.log(`My Current Balance is ${this._balance}`);
-  }
+//   getBalance() {
+//     console.log(`My Current Balance is ${this._balance}`);
+//   }
 
-  addDeposit(amount: number) {
-    this._balance = this._balance + amount;
-  }
-}
+//   addDeposit(amount: number) {
+//     this._balance = this._balance + amount;
+//   }
+// }
 
-class StudentAccount extends BankAccount {
-  test() {
-    this.addDeposit;
-  }
-}
+// class StudentAccount extends BankAccount {
+//   test() {
+//     this.addDeposit;
+//   }
+// }
 
-const myAccount = new BankAccount(444, "SKR", 20);
-console.log(myAccount);
+// const myAccount = new BankAccount(444, "SKR", 20);
+// myAccount.addDeposit(20);
+// myAccount.getBalance();
---
+++ src/getter-setter.ts
@@ -0,0 +1,41 @@
+class BankAccount {
+  public readonly id: number;
+  public name: string;
+  private _balance: number;
+
+  constructor(id: number, name: string, balance: number) {
+    this.id = id;
+    this.name = name;
+    this._balance = balance;
+  }
+
+  //getter
+  get balance(): number {
+    return this._balance;
+  }
+
+  //Setter
+
+  set deposite(amount: number) {
+    this._balance = this._balance + amount;
+  }
+
+  //   getBalance(): number {
+  //     return this._balance;
+  //   }
+
+  addDeposit(amount: number) {
+    this._balance = this._balance + amount;
+  }
+}
+
+class StudentAccount extends BankAccount {
+  test() {
+    this.addDeposit;
+  }
+}
+
+const myAccount = new BankAccount(444, "SKR", 20);
+console.log(myAccount.balance);
+myAccount.deposite = 30;
+console.log(myAccount.balance);
