b29677ff87fa9c095450214f23743e396a1b5173
Sakkar Chowdhury <sakkar.chowdhury@intellier.com>
2023-06-21 17:20:34 +0600

    static in typescript

--- src/getter-setter.ts
+++ src/getter-setter.ts
@@ -1,41 +1,41 @@
-class BankAccount {
-  public readonly id: number;
-  public name: string;
-  private _balance: number;
+// class BankAccount {
+//   public readonly id: number;
+//   public name: string;
+//   private _balance: number;
 
-  constructor(id: number, name: string, balance: number) {
-    this.id = id;
-    this.name = name;
-    this._balance = balance;
-  }
+//   constructor(id: number, name: string, balance: number) {
+//     this.id = id;
+//     this.name = name;
+//     this._balance = balance;
+//   }
 
-  //getter
-  get balance(): number {
-    return this._balance;
-  }
+//   //getter
+//   get balance(): number {
+//     return this._balance;
+//   }
 
-  //Setter
+//   //Setter
 
-  set deposite(amount: number) {
-    this._balance = this._balance + amount;
-  }
+//   set deposite(amount: number) {
+//     this._balance = this._balance + amount;
+//   }
 
-  //   getBalance(): number {
-  //     return this._balance;
-  //   }
+//   //   getBalance(): number {
+//   //     return this._balance;
+//   //   }
 
-  addDeposit(amount: number) {
-    this._balance = this._balance + amount;
-  }
-}
+//   addDeposit(amount: number) {
+//     this._balance = this._balance + amount;
+//   }
+// }
 
-class StudentAccount extends BankAccount {
-  test() {
-    this.addDeposit;
-  }
-}
+// class StudentAccount extends BankAccount {
+//   test() {
+//     this.addDeposit;
+//   }
+// }
 
-const myAccount = new BankAccount(444, "SKR", 20);
-console.log(myAccount.balance);
-myAccount.deposite = 30;
-console.log(myAccount.balance);
+// const myAccount = new BankAccount(444, "SKR", 20);
+// console.log(myAccount.balance);
+// myAccount.deposite = 30;
+// console.log(myAccount.balance);
